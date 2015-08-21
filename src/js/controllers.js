22a08814825db685fa5639a09f1d6ca89b1d9eea
manohar <manoharpalem.in@gmail.com>
2015-08-21 12:13:09 +0530

    Initial Commit with defalut project setup

---
+++ src/js/controllers.js
@@ -0,0 +1,11 @@
+angular.module('starter.controllers', [])
+
+.controller('ListCtrl', function($scope) {
+  $scope.friends = [
+  "Mother", "Mohan", "Kumar", "Venkatesh", "Dinesh",
+  "Ramu", "Naveen", "Dande", "Raja", "Pradeep", "kavitha",
+  "Mother", "Mohan", "Kumar", "Venkatesh", "Dinesh",
+  "Ramu", "Naveen", "Dande", "Raja", "Pradeep", "kavitha"
+  
+  ];
+});
